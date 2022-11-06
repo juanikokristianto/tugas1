945e343c9da76d7684a63d0fb5b9593be94a2eaf
juanikokristianto <juanikokristianto3553@gmail.com>
2022-11-06 15:30:36 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas1mobile/activity.kt
@@ -0,0 +1,11 @@
+package com.example.tugas1mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_)
+    }
+}
---
+++ app/src/main/java/com/example/tugas1mobile/activity_relative.kt
@@ -0,0 +1,11 @@
+package com.example.tugas1mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_relative : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+    }
+}
